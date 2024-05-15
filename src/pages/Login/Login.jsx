7cbee3d0bd61af3969637e155c93182865e401e4
shahriar2880 <shahriarreza436@gmail.com>
2024-05-16 03:51:44 +0600

    signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,57 +1,119 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from "react-simple-captcha";
+import { AuthContext } from "../../providers/AuthProvider";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { Helmet } from "react-helmet-async";
 
 const Login = () => {
-    const handleLogin = event =>{
-        event.preventDefault()
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
+  const [disabled, setDisabled] = useState(true);
+
+  const { signIn } = useContext(AuthContext);
+
+  useEffect(() => {
+    loadCaptchaEnginge(5);
+  });
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    signIn(email, password)
+    .then((result) => {
+      const user = result.user;
+      console.log(user);
+      Swal.fire({
+        title: "User Login Successful..",
+        icon: "success"
+      });
+    });
+  };
+  const handleValidateCaptha = (e) => {
+    const user_captcha_value = e.target.value;
+    if (validateCaptcha(user_captcha_value) == true) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
     }
+  };
+
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col md:flex-row-reverse">
-        <div className="text-center md:w-1/2 lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-        </div>
-        <div className="card  md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleLogin} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                name="email"
-                placeholder="email"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <input className="btn btn-primary" type="submit" value="Login"></input>
-            </div>
-          </form>
+    <>
+      <Helmet>
+        <title>Daily Meal || LogIn</title>
+      </Helmet>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col md:flex-row-reverse">
+          <div className="text-center  lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <LoadCanvasTemplate />
+                </label>
+                <input
+                onBlur={handleValidateCaptha}
+                  type="text"
+                  name="captcha"
+                  placeholder="type captcha here"
+                  className="input input-bordered"
+                  required
+                />
+                
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  disabled={disabled}
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Login"
+                ></input>
+              </div>
+            </form>
+            <p className="text-center pb-5">
+              <small>
+                New User? <Link to="/signup" className="text-blue-700 underline">Create An Account</Link>
+              </small>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,122 @@
+import React, { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../providers/AuthProvider";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const { createUser } = useContext(AuthContext);
+
+  const onSubmit = (value) => {
+    console.log(value);
+    createUser(value.email, value.password)
+    .then(result =>{
+        const loggedUser = result.user;
+        console.log(loggedUser);
+    })
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Daily Meal || SignUp</title>
+      </Helmet>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">SignUp Now!</h1>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("name", { required: true })}
+                  name="name"
+                  placeholder="Enter Your Name"
+                  className="input input-bordered"
+                />
+                {errors.name && (
+                  <span className="text-red-700">Name is required!</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  {...register("email", {
+                    required: true,
+                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{3,}$/i,
+                  })}
+                  name="email"
+                  placeholder="Enter Your Email"
+                  className="input input-bordered"
+                />
+                {errors.email?.type === "required" && (
+                  <span className="text-red-700">Email is required!</span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span className="text-red-700">Email pattern is wrong</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  {...register("password", {
+                    required: true,
+                    minLength: 8,
+                    maxLength: 20,
+                  })}
+                  name="password"
+                  placeholder="Enter Your Password"
+                  className="input input-bordered"
+                />
+                {errors.password?.type === "required" && (
+                  <span className="text-red-700">Password is required!</span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span className="text-red-700">
+                    Password must be 6 character long
+                  </span>
+                )}
+                {errors.password?.type === "maxLength" && (
+                  <span className="text-red-700">
+                    Password length not over 20 character long
+                  </span>
+                )}
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Sign Up"
+                />
+              </div>
+            </form>
+            <p className="text-center pb-5">
+              <small>
+                Already have an account? <Link to="/login" className="text-blue-700 underline">Please Login</Link>
+              </small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
