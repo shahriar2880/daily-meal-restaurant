dcc6fdc06a18bf9e41c7ad05b411458e724e72de
shahriar2880 <shahriarreza436@gmail.com>
2024-05-20 00:18:51 +0600

    create dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,19 +1,94 @@
-import React from 'react'
-import { NavLink, Outlet } from 'react-router-dom'
+import {
+  FaAd,
+  FaCalendar,
+  FaHome,
+  FaList,
+  FaMoneyBill,
+  FaPhone,
+  FaShoppingBag,
+  FaShoppingCart,
+} from "react-icons/fa";
+import { MdRestaurantMenu } from "react-icons/md";
+
+import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div className='flex'>
-        {/* dashboard side bar */}
-      <div className='w-64 min-h-full bg-orange-600'>
-        <ul className='menu'>
-            <li><NavLink to="/dashboard/cart">My Cart</NavLink></li>
+    <div className="flex">
+      {/* dashboard side bar */}
+      <div className="w-64 min-h-screen bg-orange-100">
+        <ul className="menu p-4 gap-1 uppercase">
+          <li>
+            <NavLink to="/dashboard/userHome">
+              <FaHome></FaHome>
+              User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/reservation">
+              <FaCalendar></FaCalendar>
+              Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/userPayment">
+              <FaMoneyBill></FaMoneyBill>
+              Payment History
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/cart">
+              <FaShoppingCart></FaShoppingCart>
+              My Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/review">
+              <FaAd></FaAd>
+              Add Review
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/bookings">
+              <FaList></FaList>
+              My Booking
+            </NavLink>
+          </li>
+        </ul>
+        <hr className="bg-black"></hr>
+        <ul className="menu p-4 gap-1 uppercase">
+          <li>
+            <NavLink to="/">
+              <FaHome></FaHome>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/menu">
+            <MdRestaurantMenu />
+              Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/shop">
+              <FaShoppingBag></FaShoppingBag>
+              Shop
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">
+              <FaPhone></FaPhone>
+              Contact
+            </NavLink>
+          </li>
         </ul>
       </div>
       {/* Dashboard content */}
-      <div className='flex-1'><Outlet></Outlet></div>
+      <div className="flex-1">
+        <Outlet></Outlet>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
