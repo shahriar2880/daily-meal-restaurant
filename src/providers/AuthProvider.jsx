aa1c7da6f39359d45b7e0a90e739c294bf7fb03d
shahriar2880 <shahriarreza436@gmail.com>
2024-05-26 23:12:44 +0600

    get token and store client

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 
 export const AuthContext = createContext(null);
@@ -12,6 +13,7 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const googleProvider = new GoogleAuthProvider();
+  const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) =>{
         setLoading(true)
@@ -42,9 +44,18 @@ const AuthProvider = ({ children }) => {
         setUser(currentUser)
         if(currentUser){
           //get token and store client
+          const userInfo = {email: currentUser.email};
+          axiosPublic.post('/jwt', userInfo)
+          .then(res =>{
+            if(res.data.token){
+              localStorage.setItem('access-tokens', res.data.token)
+
+            }
+          })
         }
         else{
           //remove token (if token stored in the client side, local storage, caching, in memory)
+          localStorage.removeItem('access-token');
         }
         setLoading(false);
     });
