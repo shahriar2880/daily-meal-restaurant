27fa0b9467e136463061143275d2da29231ed562
shahriar2880 <shahriarreza436@gmail.com>
2024-05-30 01:59:30 +0600

    admin routes

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,9 +1,8 @@
-import React, { useContext } from 'react'
-import { AuthContext } from '../providers/AuthProvider'
 import { Navigate, useLocation } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const PrivateRoutes = ({children}) => {
-    const {user, loading} = useContext(AuthContext);
+    const {user, loading} = useAuth();
     const location = useLocation()
     if(loading){
         return <progress className='progress w-56'></progress>
