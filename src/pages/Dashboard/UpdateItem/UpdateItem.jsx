bcea98b5ca96bcadfdbd578c87735d528e60178f
shahriar2880 <shahriarreza436@gmail.com>
2024-06-06 23:53:57 +0600

    create route a updateItem

--- src/pages/Dashboard/UpdateItem/UpdateItem.jsx
+++ src/pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -4,7 +4,7 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle'
 const UpdateItem = () => {
   return (
     <div>
-      <SectionTitle heading="Update Items" subHeading="Refresh Info"></SectionTitle>
+      <SectionTitle heading="Update an Items" subHeading="Refresh Info"></SectionTitle>
     </div>
   )
 }
