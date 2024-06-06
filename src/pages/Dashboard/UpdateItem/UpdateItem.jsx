c39cfca4b51f895236ba06853fab7de488afff82
shahriar2880 <shahriarreza436@gmail.com>
2024-06-06 23:26:33 +0600

    create a update items component

---
+++ src/pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle'
+
+const UpdateItem = () => {
+  return (
+    <div>
+      <SectionTitle heading="Update Items" subHeading="Refresh Info"></SectionTitle>
+    </div>
+  )
+}
+
+export default UpdateItem
