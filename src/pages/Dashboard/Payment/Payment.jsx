7dd779c9a6b1539dfdc9e9318aa46c417612d551
shahriar2880 <shahriarreza436@gmail.com>
2024-06-08 22:06:15 +0600

    Display Card Error And Create Server Side Payment Intents

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import SectionTitle from '../../../components/SectionTitle/SectionTitle'
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_Pk);
 
 const Payment = () => {
   return (
     <div>
       <SectionTitle heading="Payment" subHeading="Please pay first"></SectionTitle>
-      <div></div>
+      <div>
+        <Elements stripe={stripePromise}>
+        <CheckoutForm />
+        </Elements>
+      </div>
     </div>
   )
 }
