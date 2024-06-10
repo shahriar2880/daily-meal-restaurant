ffabadd63ca870ac80fe7f760692239f3e9cd235
shahriar2880 <shahriarreza436@gmail.com>
2024-06-11 01:22:47 +0600

    Get Order Quantity And Revenue By Category

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,15 +1,66 @@
-import ReactTypingEffect from "react-typing-effect";
 import useAuth from "../../../hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { BiMoney } from "react-icons/bi";
+import { AiFillProduct } from "react-icons/ai";
+import { GrOrderedList } from "react-icons/gr";
+import { FaUsers } from "react-icons/fa";
+import { FaVanShuttle } from "react-icons/fa6";
 
 const AdminHome = () => {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: stats ={} } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
   return (
     <div>
-      <h2>
+      <h2 className="text-3xl text-center">
         <span>Hi, Welcome </span>
         {user?.displayName ? user.displayName : "Back"}
       </h2>
-      
+      <div className="stats shadow w-full my-4">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <BiMoney/>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers/>
+          </div>
+          <div className="stat-title">Customers</div>
+          <div className="stat-value">{stats.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <AiFillProduct/>
+          </div>
+          <div className="stat-title">Products</div>
+          <div className="stat-value">{stats.menuItems}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaVanShuttle/>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
     </div>
   );
 };
