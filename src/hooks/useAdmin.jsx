369f426242356c46960d8c2d55b7b078cf14819b
shahriar2880 <shahriarreza436@gmail.com>
2024-06-10 12:14:09 +0600

    Fix Login Issue And API Race Condition

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -4,10 +4,11 @@ import useAxiosSecure from "./useAxiosSecure";
 import useAuth from "./useAuth";
 
 const useAdmin = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const axiosSecure = useAxiosSecure();
   const { data: isAdmin, isPending: isAdminLoading } = useQuery({
       queryKey: [user?.email, 'isAdmin'],
+      enabled: loading,
       queryFn: async () => {
           console.log('asking or checking is admin', user)
           const res = await axiosSecure.get(`/users/admin/${user.email}`);
