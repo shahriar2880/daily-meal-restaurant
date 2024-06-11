2f99a9236d3120b394835eb87e02cfeeaaf74697
shahriar2880 <shahriarreza436@gmail.com>
2024-06-11 19:03:56 +0600

    updated

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,155 +1,118 @@
-import useAuth from "../../../hooks/useAuth";
-import { useQuery } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-import { BiMoney } from "react-icons/bi";
-import { AiFillProduct } from "react-icons/ai";
-import { GrOrderedList } from "react-icons/gr";
-import { FaUsers } from "react-icons/fa";
-import { FaVanShuttle } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
-const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
-
-const AdminHome = () => {
-  const { user } = useAuth();
-  const axiosSecure = useAxiosSecure();
-
-  const { data: stats = {} } = useQuery({
-    queryKey: ["admin-stats"],
-    queryFn: async () => {
-      const res = await axiosSecure.get("/admin-stats");
-      return res.data;
-    },
-  });
-
-  const { data: chartData = [] } = useQuery({
-    queryKey: ["order-stats"],
-    queryFn: async () => {
-      const res = await axiosSecure.get("/order-stats");
-      return res.data;
-    },
-  });
-
-  /// custom shape for the bar chart
-  const getPath = (x, y, width, height) => {
-    return `M${x},${y + height}C${x + width / 3},${y + height} ${
-      x + width / 2
-    },${y + height / 3}
-    ${x + width / 2}, ${y}
-    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
-      x + width
-    }, ${y + height}
-    Z`;
-  };
-
-  const TriangleBar = (props) => {
-    const { fill, x, y, width, height } = props;
-
-    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-  };
-
-  // custom shape for the pie chart
-  const RADIAN = Math.PI / 180;
-  const renderCustomizedLabel = ({
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    percent,
-  }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+const AddItems = () => {
+    const { register, handleSubmit, reset } = useForm();
+    const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
+    const onSubmit = async (data) => {
+        console.log(data)
+        // image upload to imgbb and then get an url
+        const imageFile = { image: data.image[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        });
+        if (res.data.success) {
+            // now send the menu item data to the server with the image url
+            const menuItem = {
+                name: data.name,
+                category: data.category,
+                price: parseFloat(data.price),
+                recipe: data.recipe,
+                image: res.data.data.display_url
+            }
+            // 
+            const menuRes = await axiosSecure.post('/menu', menuItem);
+            console.log(menuRes.data)
+            if(menuRes.data.insertedId){
+                // show success popup
+                reset();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${data.name} is added to the menu.`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        }
+        console.log( 'with image url', res.data);
+    };
 
     return (
-      <text
-        x={x}
-        y={y}
-        fill="white"
-        textAnchor={x > cx ? "start" : "end"}
-        dominantBaseline="central"
-      >
-        {`${(percent * 100).toFixed(0)}%`}
-      </text>
+        <div>
+            <SectionTitle heading="add an item" subHeading="What's new?" ></SectionTitle>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-control w-full my-6">
+                        <label className="label">
+                            <span className="label-text">Recipe Name*</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Recipe Name"
+                            {...register('name', { required: true })}
+                            required
+                            className="input input-bordered w-full" />
+                    </div>
+                    <div className="flex gap-6">
+                        {/* category */}
+                        <div className="form-control w-full my-6">
+                            <label className="label">
+                                <span className="label-text">Category*</span>
+                            </label>
+                            <select defaultValue="default" {...register('category', { required: true })}
+                                className="select select-bordered w-full">
+                                <option disabled value="default">Select a category</option>
+                                <option value="salad">Salad</option>
+                                <option value="pizza">Pizza</option>
+                                <option value="soup">Soup</option>
+                                <option value="dessert">Dessert</option>
+                                <option value="drinks">Drinks</option>
+                            </select>
+                        </div>
+
+                        {/* price */}
+                        <div className="form-control w-full my-6">
+                            <label className="label">
+                                <span className="label-text">Price*</span>
+                            </label>
+                            <input
+                                type="number"
+                                placeholder="Price"
+                                {...register('price', { required: true })}
+                                className="input input-bordered w-full" />
+                        </div>
+
+                    </div>
+                    {/* recipe details */}
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Recipe Details</span>
+                        </label>
+                        <textarea {...register('recipe')} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+                    </div>
+
+                    <div className="form-control w-full my-6">
+                        <input {...register('image', { required: true })} type="file" className="file-input w-full max-w-xs" />
+                    </div>
+
+                    <button className="btn">
+                        Add Item <FaUtensils className="ml-4"></FaUtensils>
+                    </button>
+                </form>
+            </div>
+        </div>
     );
-  };
-
-  const pieChartData = chartData.map((data) => {
-    return { name: data.category, value: data.revenue };
-  });
-
-  return (
-    <div>
-      <h2 className="text-3xl text-center">
-        <span>Hi, Welcome </span>
-        {user?.displayName ? user.displayName : "Back"}
-      </h2>
-      <div className="stats shadow w-full my-4">
-        <div className="stat">
-          <div className="stat-figure text-secondary">
-            <BiMoney />
-          </div>
-          <div className="stat-title">Revenue</div>
-          <div className="stat-value">${stats.revenue}</div>
-          <div className="stat-desc">Jan 1st - Feb 1st</div>
-        </div>
-
-        <div className="stat">
-          <div className="stat-figure text-secondary">
-            <FaUsers />
-          </div>
-          <div className="stat-title">Customers</div>
-          <div className="stat-value">{stats.users}</div>
-          <div className="stat-desc">↗︎ 400 (22%)</div>
-        </div>
-
-        <div className="stat">
-          <div className="stat-figure text-secondary">
-            <AiFillProduct />
-          </div>
-          <div className="stat-title">Products</div>
-          <div className="stat-value">{stats.menuItems}</div>
-          <div className="stat-desc">↘︎ 90 (14%)</div>
-        </div>
-
-        <div className="stat">
-          <div className="stat-figure text-secondary">
-            <FaVanShuttle />
-          </div>
-          <div className="stat-title">Orders</div>
-          <div className="stat-value">{stats.orders}</div>
-          <div className="stat-desc">↘︎ 90 (14%)</div>
-        </div>
-      </div>
-      <div className="flex">
-        <div className="w-1/2">
-        <BarChart
-      width={500}
-      height={300}
-      data={chartData}
-      margin={{
-        top: 20,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-        {pieChartData.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-        ))}
-      </Bar>
-    </BarChart>
-        </div>
-        <div className="w-1/2"></div>
-      </div>
-    </div>
-  );
 };
 
-export default AdminHome;
+export default AddItems;
